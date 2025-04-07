762d987dc3fe015443857720aaff185fddbd76a0
Carson Bell <cjb83156@uga.edu>
2025-04-07 18:30:05 -0400

    Working Login and signup w/ hashing

--- biblio-byte/src/models/User.js
+++ biblio-byte/src/models/User.js
@@ -1,8 +1,44 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true,
+    },
 });
 
+userSchema.pre('save', async function(next) {
+    try {
+      // Check if the password has been modified
+      if (!this.isModified('password')) return next();
+      
+      // Generate a salt and hash the password
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      
+      next(); // Proceed to save
+    } catch (error) {
+      next(error); // Pass any errors to the next middleware
+    }
+  });
+
+
+
 export default mongoose.models.User || mongoose.model("User", userSchema);
+
