b13d174c30c4bea86cb92cf9a90e3c68489eaf85
Carson Bell <cjb83156@uga.edu>
2025-04-09 12:21:37 -0400

    working on book backend

--- biblio-byte/src/app/api/books/route.js
+++ biblio-byte/src/app/api/books/route.js
@@ -1,3 +1,7 @@
 import Book from "../../../models/Book.js";
 import connectMongoDB from "../../../../config/mongodb.ts";
 
+export async function POST(req) {
+    const {  } = await req.json();
+
+    await connectMongoDB();
