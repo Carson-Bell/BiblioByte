b79d53bbcd245307a018bcc70ef36e06d44e7ff4
Carson Bell <cjb83156@uga.edu>
2025-04-09 21:34:37 -0400

    Edit Book mod for reviews

--- biblio-byte/src/models/Book.js
+++ biblio-byte/src/models/Book.js
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema({
+    reviewer: {
+        type: String, // Name or ID of the reviewer
+        required: true,
+        trim: true,
+    },
+    rating: {
+        type: Number, // Rating out of 5
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    comment: {
+        type: String, // Optional comment
+        trim: true,
+    },
+    date: {
+        type: Date, // Date of the review
+        default: Date.now,
+    },
+});
+
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -21,6 +43,7 @@ const bookSchema = new mongoose.Schema({
         required: true,
         trim: true,
     },
+    reviews: [reviewSchema], // Array of reviews
 
 });
 
