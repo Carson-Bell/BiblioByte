d921ddb68222ff8f10463643861803f15e804a6e
Rachel Mende <rmm29724@uga.edu>
2025-04-21 13:20:26 -0400

    mobile book page header

--- biblio-byte/src/components/BookPageHeader.js
+++ biblio-byte/src/components/BookPageHeader.js
@@ -1,12 +1,31 @@
 'use client';
 //TEST CHANGE FROM BRIANNA
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function BookPageHeader() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768); // Set mobile breakpoint
+        };
+
+        // Initial check
+        handleResize();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <>
-            <nav className="w-full text-white py-2 px-4 fixed top-[64px] z-30 shadow-md"
-             style={{ backgroundColor: 'rgba(11,79,74,1)'}}>
+            <nav className={`w-full text-white py-2 px-4 fixed z-30 shadow-md ${
+                    isMobile ? "top-[54px]" : "top-[66px]"
+                }`}
+                style={{ backgroundColor: 'rgba(11,79,74,1)' }}
+             >
                 <ul className="list-none grid grid-cols-3 gap-3 text-center">
                     <li>
                         <a href="#info" className="hover:underline">
@@ -29,4 +48,5 @@ export default function BookPageHeader() {
             <div className="w-full h-1 bg-white shadow-md"></div>
         </>
     );
+
 }
